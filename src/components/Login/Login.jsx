a59f011d6d00d6b81529528e006537d2d1af9a9c
ayush <ayushsrivastava16252@gmail.com>
2023-11-01 09:52:25 +0530

    file added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,16 +7,17 @@ const Login = () => {
     const [inputValue, setInputValue] = useState("");
 
     const handleClick = () => {
-        // Remove any whitespace from the phone number input
+
         const phoneNumber = inputValue.replace(/\s/g, '');
 
-        // Check if the phone number is valid (e.g., 10 digits)
+
         if (phoneNumber.length !== 10) {
             alert('Number is invalid');
             return;
         } else {
-            // If the phone number is valid, navigate to the OTP page
-            window.location.href = `/otp?phoneNumber=${inputValue}`; // You can use window.location.href to navigate
+
+            window.location.href = `/otp?phoneNumber=${inputValue
+                }`;
         }
     };
     return (
@@ -40,7 +41,7 @@ const Login = () => {
                             <input placeholder='Enter your Phone Number' onChange={(e) => setInputValue(e.target.value)} value={inputValue} type="number" />
                         </div>
                         <div className='otp' onClick={handleClick}>
-                            {/* Use Link to navigate to the OTP page conditionally based on the phone number validity */}
+
                             {inputValue.length === 10 ? (
                                 <Link to="/otp">
                                     <p>Send OTP</p>
